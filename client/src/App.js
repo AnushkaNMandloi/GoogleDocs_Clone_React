7a6fbb41157e02c1ae7f63c456dab6c4834d32a8
AnushkaNMandloi <123271785+AnushkaNMandloi@users.noreply.github.com>
2023-01-21 16:19:13 -0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+//import './App.css';
+import TextEditor from './TextEditor';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import {v4 as uuidV4} from "uuid";
+
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`}/>} exact> 
+  {/* to generate random document id */}
+      </Route>
+      <Route path="/documents/:id" element={<TextEditor />}>
+      </Route>
+    </Routes>
+  </Router>
+}
+
+export default App;
---
+++ client/src/frontend/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <img src="">
+        Doc 1
+      </img>
+
+      <img src="">
+        Doc 2
+      </img>
+    </div>
+  )
+}
+
+export default Dashboard
